6d6e9323fa392605f66ebc2fcecfea426fdfb6d0
Albert <albertzayn7@gmail.com>
2023-07-06 01:21:19 +0700

    dd extra markup and styling for Explore page
    Added revritecondition for diff type of images.
    User Story 1 Explore page

--- dotwitter/app/Views/explore.php
+++ dotwitter/app/Views/explore.php
@@ -1,5 +1,5 @@
 <?php
-    $title = 'Explore / dotwitter';
+$title = 'Explore / dotwitter';
 ?>
 
 <html>
@@ -9,16 +9,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title; ?></title>
-    <!-- Favicon -->
-    <link rel="shortcut icon" href="/public/images/dotwitter-favicon.png" type="image/png">
     <!-- CSS -->
     <link rel="stylesheet" href="/public/styles.css">
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
+            integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
     </script>
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="/public/images/favicon32x.png" type="image/png">
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -42,6 +42,12 @@
         background-color: #1DA1F2;
     }
 
+    .birdSide-bg-img {
+        height: 100%;
+        width: 100%;
+        z-index: -1;
+    }
+
     .loginSide {
         padding: 20px;
     }
@@ -52,10 +58,6 @@
         display: flex;
         flex-direction: column;
         justify-content: center;
-    }
-
-    .dotwitter-logo {
-        padding-bottom: 12px;
     }
 
     h1 {
@@ -104,54 +106,80 @@
         color: rgb(185, 202, 211)
     }
 
-    span {
-        padding: 0 10px 0;
-    }
+
 
     .signUp {
         width: 300px;
         border-radius: 24px;
         background-color: rgb(29, 155, 240);
         color: #fff;
-        letter-spacing: -1px;
+        letter-spacing: -0.5px;
         margin-bottom: 8px;
         height: 40px;
         border-style: none;
         font-size: 16px;
-    }
+        font-weight: 700;
+     }
+
     .signUp:hover {
         background-color: rgb(26, 140, 216);
     }
 
+    .logIn-bottom {
+        position: relative;
+        display: inline-block;
+        padding: 40px 0 20px;
+        font-weight: 700;
+    }
+
+    .logIn-btn {
+        width: 300px;
+        border-radius: 24px;
+        border-style: double;
+        border-color: rgb(223, 230, 234);
+        background-color: #fff;
+        color: rgb(29, 155, 240);
+        font-weight: 700;
+        letter-spacing: -1px;
+        margin-bottom: 8px;
+        height: 40px;
+        font-size: 17px;
+        letter-spacing: -0.5px;
+    }
+
 </style>
+
 </head>
 
 <body>
-    <div class="grid-container">
-        <section class="birdSide">
-            <img src="/public/images/birdSide.png" alt="#">
-        </section>
-        <section class="loginSide">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <img class="dotwitter-logo" src="/public/images/dotwitter-logo.png" alt="#">
-                        <h1>Happening now</h1>
-                        <h2>Join Twitter today</h2>
-                        <input type="text" id="username" placeholder="  Username">
-                        <input type="password" id="password" placeholder="  Password">
-                        <div class="or">
-                            <hr><span>or</span>
-                            <hr><br>
-                        </div>
-                        <button class="signUp">Create account</button>
-                        <span>Already have an account?</span>
-                        <button class="logIn">Log in</button>
+<div class="grid-container">
+    <section class="birdSide">
+        <img src="/public/images/birdSide.png" class="img-fluid birdSide-bg-img" alt="#">
+    </section>
+    <section class="loginSide">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <img src="/public/images/favicon96x.png" height="96" width="96" class="img-fluid" alt="...">
+                    <h1>Happening now</h1>
+                    <h2>Join Twitter today</h2>
+                    <input type="text" id="username" placeholder="  Username">
+                    <input type="password" id="password" placeholder="  Password">
+                    <div class="or">
+                        <hr>
+                        <span>or</span>
+                        <hr>
+                        <br>
+                    </div>
+                    <button class="signUp">Create account</button>
+                    <div class="logIn-bottom">
+                        <span>Already have an account?</span><br>
+                        <button class="logIn-btn">Log in</button>
                     </div>
                 </div>
             </div>
-        </section>
-    </div>
+    </section>
+</div>
 </body>
 
 </html>
