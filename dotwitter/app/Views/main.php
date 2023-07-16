2bc7418481e5881b8920a77d94f0107656a56f85
Albert <albertzayn7@gmail.com>
2023-07-17 01:10:08 +0700

    Add responsivity, footer, redirect for main page
    Added footer.
    Revorked inputs, added `agreement` phrase to the page.
    Added redirecting option to the `Explore` button.
    
    US-1, US-9, responsitivity

--- dotwitter/app/Views/main.php
+++ dotwitter/app/Views/main.php
@@ -24,57 +24,84 @@ $title = 'Main / dotwitter';
     <link href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;700;800;900&display=swap" rel="stylesheet">
     <!-- CSS -->
     <link rel="stylesheet" href="/styles.css">
+    <!-- JavaScript -->
+    <script>
+        function explore() {
+            window.location.href = 'http://45.9.41.41/explore';
+        }
+    </script>
 </head>
 
 <body>
-<div class="grid-container">
-    <section class="birdSide">
-        <div class="birdSide-container">
-            <img src="/images/birdSide.png" class="img-fluid birdSide-bg-img" alt="#">
-            <svg class="img-fluid birdSide-fr-img" xmlns="http://www.w3.org/2000/svg" width="359" height="292"
-                 viewBox="0 0 512 512">
-                <style>svg {
-                        fill: #ffffff
-                    }</style>
-                <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/>
-            </svg>
+<div class="grid-container container-fluid">
+    <div class="row">
+        <div class="col col-lg-6">
+            <section class="birdSide">
+                <div class="birdSide-container">
+                    <img src="/images/birdSide.png" class="img-fluid birdSide-bg-img" alt="#">
+                    <svg class="img-fluid birdSide-fr-img" xmlns="http://www.w3.org/2000/svg" width="359" height="292"
+                         viewBox="0 0 512 512">
+                        <style>svg {
+                                fill: #ffffff
+                            }</style>
+                        <path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/>
+                    </svg>
+                </div>
+            </section>
         </div>
-    </section>
-    <section class="loginSide">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <img src="https://cdn.iconscout.com/icon/free/png-256/free-twitter-241-721979.png" height="34.2"
-                         width="42.6" class="img-fluid" alt="...">
-                    <h1>Happening now</h1>
-                    <h2>Join dotwitter today</h2>
-                    <div class="loginSide-functionality">
-                        <input type="text" id="username" maxlength="50" placeholder="  Username" onfocus="" onblur="this.placeholder">
-                        <input type="password" id="password" ondragstart="" placeholder="  Password" onfocus="" onblur="this.placeholder">
-                        <button class="logIn-btn">Log In</button>
-                        <div class="or">
-                            <hr>
-                            <span class="or-span">or</span>
-                            <hr>
-                            <br>
-                        </div>
-                        <div class="SignUp-question">
-                            <span>Don't have an account?</span>
-                            <span>Want to explore without registration?</span>
-                        </div>
-                        <div class="SignUp-btn-box">
-                            <button class="SignUp-btn">Create account</button>
-                            <button class="SignUp-btn">Explore</button>
+        <div class="col col-lg-6">
+            <section class="loginSide">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <img src="https://cdn.iconscout.com/icon/free/png-256/free-twitter-241-721979.png"
+                                 height="34.2"
+                                 width="42.6" class="img-fluid" alt="...">
+                            <h1>Happening now</h1>
+                            <h2>Join dotwitter today</h2>
+                            <div class="loginSide-functionality">
+                                <input type="text" id="username" minlength="6" maxlength="50" placeholder="Username"
+                                       onfocus="" onblur="this.placeholder">
+                                <input type="password" id="password" minlength="6" maxlength="50" placeholder="Password"
+                                       onfocus="" onblur="this.placeholder">
+                                <button class="logIn-btn">Log In</button>
+                                <div class="or">
+                                    <hr>
+                                    <span class="or-span">or</span>
+                                    <hr>
+                                    <br>
+                                </div>
+                                <div class="container-fluid">
+                                    <div class="SignUp-btn-box row">
+                                        <div class="col-lg-6">
+                                            <span class="SignUp-question">Don't have an account?</span>
+                                            <button class="SignUp-btn">Create account</button>
+                                            <br>
+                                            <div class="SignUp-agreement">
+                                                <span> By signing up, you are signing an <a
+                                                            style="cursor: pointer; color: rgb(29, 155, 240); text-decoration: none;"
+                                                            href="#">Agreement</a>, that you are not stuffy or nitpicker person</span>
+                                            </div>
+                                        </div>
+                                        <div class="col-lg-6">
+                                            <span class="SignUp-question">Explore without registration?</span>
+                                            <button class="SignUp-btn" onclick="explore()">Explore</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <footer>
+                                <div>
+                                    <span class="Footer-spans">About</span>
+                                    <span class="Footer-spans">Agreement</span>
+                                    <span class="Footer-spans">© 2023 .wrk</span>
+                                </div>
+                            </footer>
                         </div>
                     </div>
-                    <footer>
-                        <div>
-
-                        </div>
-                    </footer>
-                </div>
-            </div>
-    </section>
+            </section>
+        </div>
+    </div>
 </div>
 </body>
 
