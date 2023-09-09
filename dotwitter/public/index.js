f39b2064e42c5f77d944462edc6bbcdf1557da2b
Albert <albertzayn7@gmail.com>
2023-09-10 02:02:20 +0700

    TASK 18 - Optimized 2nd part of js code

--- dotwitter/public/index.js
+++ dotwitter/public/index.js
@@ -57,93 +57,67 @@ const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]
 const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
 // ******* MAIN PAGE *********
-const fullnameInput = document.getElementById('fullname');
-const fullnameFeedback = document.getElementsByClassName('ValidationFullname');
 
-fullnameInput.addEventListener('input', () => {
-    if (fullnameInput.value.length >= 1) {
-        fullnameInput.classList.add('is-valid');
-        fullnameInput.classList.remove('is-invalid');
-        fullnameFeedback.style.display = 'none';
-    } else {
-        fullnameInput.classList.remove('is-valid');
-        fullnameInput.classList.add('is-invalid');
-        fullnameFeedback.style.display = 'block';
-    }
-});
+function updateValidation(inputElement, isValid, feedbackElement) {
+    inputElement.classList.toggle('is-valid', isValid);
+    inputElement.classList.toggle('is-invalid', !isValid);
+    feedbackElement.style.display = isValid ? 'none' : 'block';
+}
 
-const emailInput = document.getElementById('email');
-const emailFeedback = document.getElementsByClassName('ValidationEmail');
+function validateInput(inputElement, validationFunction, feedbackElement) {
+    inputElement.addEventListener('input', () => {
+        const isValid = validationFunction(inputElement.value);
+        updateValidation(inputElement, isValid, feedbackElement);
+        validateStep1();
+    });
+}
 
-emailInput.addEventListener('input', () => {
+function validateFullname(fullname) {
+    return fullname.length >= 1;
+}
+
+function validateEmail(email) {
     const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (emailPattern.test(emailInput.value)) {
-        emailInput.classList.add('is-valid');
-        emailInput.classList.remove('is-invalid');
-        emailFeedback.style.display = 'none';
-    } else {
-        emailInput.classList.remove('is-valid');
-        emailInput.classList.add('is-invalid');
-        emailFeedback.style.display = 'block';
-    }
-});
+    return emailPattern.test(email);
+}
 
+function validateUsername(username) {
+    return username.length >= 1;
+}
+
+function validatePassword(password) {
+    return password.length >= 8;
+}
+
+const fullnameInput = document.getElementById('fullname');
+const fullnameFeedback = document.querySelector('.ValidationFullname');
+const emailInput = document.getElementById('email');
+const emailFeedback = document.querySelector('.ValidationEmail');
 const usernameInput = document.getElementById('username');
-const usernameFeedback = document.getElementsByClassName('ValidationUsername');
-
-usernameInput.addEventListener('input', () => {
-    if (usernameInput.value.length >= 1) {
-        usernameInput.classList.add('is-valid');
-        usernameInput.classList.remove('is-invalid');
-        usernameFeedback.style.display = 'none';
-    } else {
-        usernameInput.classList.remove('is-valid');
-        usernameInput.classList.add('is-invalid');
-        usernameFeedback.style.display = 'block';
-    }
-});
-
+const usernameFeedback = document.querySelector('.ValidationUsername');
 const nextButton = document.querySelector('.step1-next-btn');
+const createAccButton = document.querySelector('.step2-next-btn');
+const passwordInput = document.getElementById('password');
+const passwordFeedback = document.querySelector('.ValidationPassword');
 
-fullnameInput.addEventListener('input', validateStep1);
-emailInput.addEventListener('input', validateStep1);
-usernameInput.addEventListener('input', validateStep1);
+validateInput(fullnameInput, validateFullname, fullnameFeedback);
+validateInput(emailInput, validateEmail, emailFeedback);
+validateInput(usernameInput, validateUsername, usernameFeedback);
+validateInput(passwordInput, validatePassword, passwordFeedback);
+validateStep1();
 
 function validateStep1() {
-    const isFullnameValid = fullnameInput.value.length >= 1;
-    const isEmailValid = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(emailInput.value);
-    const isUsernameValid = usernameInput.value.length >= 1;
+    const isFullnameValid = validateFullname(fullnameInput.value);
+    const isEmailValid = validateEmail(emailInput.value);
+    const isUsernameValid = validateUsername(usernameInput.value);
 
-    if (isFullnameValid && isEmailValid && isUsernameValid) {
-        nextButton.removeAttribute('disabled');
-    } else {
-        nextButton.setAttribute('disabled', 'true');
-    }
+    nextButton.disabled = !(isFullnameValid && isEmailValid && isUsernameValid);
 }
-
-const passwordInput = document.getElementById('password');
-const passwordFeedback = document.getElementsByClassName('ValidationPassword');
-
-passwordInput.addEventListener('input', () => {
-    if (passwordInput.value.length >= 8) {
-        passwordInput.classList.add('is-valid');
-        passwordInput.classList.remove('is-invalid');
-        passwordFeedback.style.display = 'none';
-    } else {
-        passwordInput.classList.remove('is-valid');
-        passwordInput.classList.add('is-invalid');
-        passwordFeedback.style.display = 'block';
-    }
-});
-
-const createAccButton = document.querySelector('.step2-next-btn');
-passwordInput.addEventListener('input', validateStep2);
 
 function validateStep2() {
-    const isPasswordValid = passwordInput.value.length >= 8;
-    if (isPasswordValid) {
-        createAccButton.removeAttribute('disabled');
-    } else {
-        createAccButton.setAttribute('disabled', 'true');
-    }
+    const isPasswordValid = validatePassword(passwordInput.value);
+    createAccButton.disabled = !isPasswordValid;
+    validateInput(passwordInput, validatePassword, passwordFeedback);
+    passwordInput.addEventListener('input', validateStep2);
 }
+validateStep2();
